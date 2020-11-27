027dd8cf36ab13093e1b174f835871d4957a1951
alexander <coding@aggrosoft.de>
2020-11-27 09:51:08 +0100

    add translation

---
+++ Application/views/admin/de/agcanonicalurls_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$aLang = array(
+    'charset'                                   => 'UTF-8',
+    'CUSTOM_CANONICAL_URL' => 'Spezielle Canonical Url',
+    'HELP_CUSTOM_CANONICAL_URL' => 'Ersetzt die Canonical Url mit dem eingetragenen Wert, verwenden Sie hier unbedingt absolute Urls (e.g. https://www.meinedomain.de/pfad/artikel.html)',
+);
